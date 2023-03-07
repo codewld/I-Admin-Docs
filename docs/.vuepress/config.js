a81436e45e5fd4b46347d2b327bf6c976b6dff15
codewld <wang_longda@outlook.com>
2023-03-07 15:05:31 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  lang: 'zh-CN',
+  title: 'I-Admin-Docs',
+  description: 'I-Admin = "少代码" = Spring Boot 后端 + Vue3 后台',
+  base: '/I-Admin-Docs/',
+  themeConfig: {
+    repo: 'codewld/I-Admin',
+    sidebar: [
+      {text: '介绍', link: '/'},
+      {text: '技术栈', link: '/technology-stack/'},
+      {text: '更新日志', link: '/change-log/'}
+    ],
+    sidebarDepth: 0,
+    editLink: false,
+    contributors: false
+  }
+}
---
+++ docs/technology-stack/index.md
@@ -0,0 +1,27 @@
+# 技术栈
+
+## I-Admin-Server
+
+* Spring Boot
+* Spring Security
+* JWT
+* MyBatis Plus
+* MySQL
+* Redis
+* Validator
+* Lombok
+* Swagger
+* MyBatis Plus Generator
+
+
+
+## I-Mall-Admin
+
+* Vue3
+* Vite
+* TypeScript
+* Vue Router
+* Tailwind CSS
+* Element Plus
+* axios
+* pinia
