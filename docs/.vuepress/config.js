666089ec1cf24cef9f3226762d60d2dfc04e8e0b
codewld <wang_longda@outlook.com>
2023-04-07 22:16:05 +0800

    feat: 增加前端组件库说明

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,16 @@ module.exports = {
     sidebar: [
       {text: '介绍', link: '/'},
       {text: '技术栈', link: '/technology-stack/'},
-      {text: '更新日志', link: '/change-log/'}
+      {text: '更新日志', link: '/change-log/'},
+      {
+        text: '前端组件库',
+        children: [
+          {text: 'iCrud', link: '/front-end-components/iCrud'},
+          {text: 'iContainer', link: '/front-end-components/iContainer'},
+          {text: 'iCard', link: '/front-end-components/iCard'},
+          {text: 'iStatus', link: '/front-end-components/iStatus'}
+        ]
+      },
     ],
     sidebarDepth: 0,
     editLink: false,
---
+++ docs/front-end-components/iContainer.md
@@ -0,0 +1,14 @@
+# iContainer - 容器组件
+
+## 特点
+
+* 带滚动带间距的垂直布局容器，由 `el-scrollbar + el-space` 组成
+* jsx 形式，避免 el-space 误处理导致注释占据空间
+
+
+
+
+
+## Slots
+
+* default - 内容区
